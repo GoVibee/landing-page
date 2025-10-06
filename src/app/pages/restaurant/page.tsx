4575427a43d58391ebedba86be67d251387f2918
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2025-10-06 11:36:04 +0000

    updated restaurant

--- src/app/pages/restaurant/page.tsx
+++ src/app/pages/restaurant/page.tsx
@@ -158,13 +158,29 @@ export default function CreateRestaurantPage() {
                                 {category.items.map((item, itemIndex) => (
                                   <div key={item.id}>
                                     <div className="grid grid-cols-1 md:grid-cols-12 gap-3">
-                                        <input type="text" placeholder={`Item ${itemIndex + 1} Name`} className="md:col-span-5 px-3 py-2 border border-gray-300 rounded-md text-sm" />
-                                        <input type="text" placeholder="Price" className="md:col-span-2 px-3 py-2 border border-gray-300 rounded-md text-sm" />
+                                        <input type="text" placeholder={`Item ${itemIndex + 1} Name`} className="md:col-span-7 px-3 py-2 focus:outline-none focus:ring-[#3B0A45] focus:ring-2 border border-gray-300 rounded-md text-sm" />
+                                        <input type="text" placeholder="Price" className="md:col-span-4 px-3 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:ring-[#3B0A45] focus:ring-2" />
                                        
                                         <button type="button" onClick={() => removeMenuItem(category.id, item.id)} className="md:col-span-1 flex justify-center items-center text-red-500 hover:text-red-700 cursor-pointer"><Trash2 size={16} /></button>
                                     </div>
-                                    <div className='mt-5'>
+                                    <div className='my-5'>
                                        <TextAreaField label="Description(Optional)" id="description" placeholder="A juicy burger with melted cheese and crispy fries" />
+                                    </div>
+                                    <div className='mt-5'>
+                                       <p className="block text-sm font-medium text-gray-700 mb-1">Upload menu category image</p>
+                                      <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                                        <div className="text-center">
+                                          <UploadCloud className="mx-auto h-12 w-12 text-gray-400" />
+                                          <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                                            <label htmlFor="file-upload" className="relative cursor-pointer rounded-md bg-white font-semibold text-gray-800 focus-within:outline-none focus-within:ring-2 focus-within:ring-gray-700 focus-within:ring-offset-2 hover:text-gray-900">
+                                              <span>Upload files</span>
+                                              <input id="file-upload" name="file-upload" type="file" className="sr-only" multiple />
+                                            </label>
+                                            <p className="pl-1">or drag and drop</p>
+                                          </div>
+                                          <p className="text-xs leading-5 text-gray-500">PNG, JPG, GIF up to 10MB; MP4 up to 50MB</p>
+                                        </div>
+                                      </div>
                                     </div>
                                   </div>
                                 ))}
